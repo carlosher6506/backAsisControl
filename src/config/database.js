77807e37a6d7b10c213e5bf184623a5bb2e1b191
Gohe <juancarlos.gomez@ulv.edu.mx>
2026-04-15 00:39:47 -0500

    fix(supabase): create conexion to supabase up

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,12 @@ const {Pool} = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL});
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  family: 4
+});
 
 
 pool.connect().then(() => console.log('Connected to the database successfully!'))
