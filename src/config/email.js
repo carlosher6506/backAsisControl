6b9f7561e8b237250ba42ddb7024f05b77891ab8
Gohe <juancarlos.gomez@ulv.edu.mx>
2026-06-15 12:51:35 -0500

    fix(reset): modify transporter

--- src/config/email.js
+++ src/config/email.js
@@ -3,10 +3,21 @@ require('dotenv').config();
 
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+    }
+});
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.error("SMTP ERROR:", error);
+    } else {
+        console.log("SMTP OK");
     }
 });
 
